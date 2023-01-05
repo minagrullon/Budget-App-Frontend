c5800e1ad2c6e92fca9a6cd62355651a7099fb14
minagrullon <minagrullon@pursuit.org>
2023-01-05 13:08:35 -0500

    added show page

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,5 +1,48 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+const API = process.env.REACT_APP_API_URL;
 
 export default function Show() {
-  return <div>Show</div>;
+  const { index } = useParams();
+  const navigate = useNavigate();
+  const [transaction, setTransaction] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${API}/transactions/${index}`)
+      .then((res) => {
+        setTransaction(res.data);
+      })
+      .catch((err) => navigate("/*"));
+  }, []);
+
+  function deleteTransaction() {
+    axios
+      .delete(`${API}/transactions/${index}`)
+      .then(() => {
+        navigate("/transactions");
+      })
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <div>
+      <h2>Show</h2>
+      <div className="transaction_details" style={{ textAlign: "center" }}>
+        <h3>{transaction.item_name}</h3>
+        <h4>${transaction.amount}</h4>
+        <h5>{transaction.date}</h5>
+        <h5>{transaction.from}</h5>
+      </div>
+      <div className="show_page_buttons">
+        <button onClick={() => navigate("/transactions")}>Back</button>
+        <button onClick={() => navigate(`/transactions/${index}/edit`)}>
+          Edit
+        </button>
+        <button onClick={deleteTransaction}>Delete</button>
+      </div>
+    </div>
+  );
 }
